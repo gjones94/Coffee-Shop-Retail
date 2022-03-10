fac9eca8bf54a45d9d045ec613a2631035f20774
Ubuntu <ubuntu@ip-172-31-92-131.ec2.internal>
2022-03-10 02:52:57 +0000

    adding updated index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,14 @@ const mysql = require("mysql");
 
 var check;
 
-app.use(express.static(path.resolve('/var/www/html/')));
+const build_directory = path.join(__dirname, '../build');
 
-const db = mysql.createPool({
+
+const db = mysql.createConnection({
 host: 'localhost',
 user: 'root',
-password: 'password',
-database: 'beansdatabase',
+password: 'DB@dmin422!',
+database: 'store',
 });
 
 //used to translate files between backend and frontend
@@ -22,20 +23,21 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(build_directory));
 
 //post function to get login input from front
 app.post('/api/login/auth',(req,res) =>{
     const tempname = req.body.PassName
     const tempPW = req.body.PassPW
-   
-    sqlFind = "SELECT Name FROM users WHERE Name = ?;" //command to mysql to find user name from user table
+    console.log("Post called") 
+    sqlFind = "SELECT user_first_name FROM users WHERE user_first_name = ?;" //command to mysql to find user name from user table
     db.query(sqlFind,[tempname],(err,result) => {
             if ( result == ""){ //if the result is empty that means nothing was found
                 res.send("Not Found")
             }else {
             
 
-                sqlFind = "SELECT Password FROM users WHERE Password = ?;" // checks through password after name
+                sqlFind = "SELECT password FROM users WHERE password = ?;" // checks through password after name
                 db.query(sqlFind,[tempPW],(err,result) => {
                     if (result == ""){
                         res.send("Not Found")
@@ -55,7 +57,7 @@ app.get('/api/get', (req,res) => {
     console.log("basic get called")
     const sqlSelect = "SELECT * FROM users"; //mysql command to get full list of users
     db.query(sqlSelect,(err,result) =>{
-	res.send("Test")
+	//res.send("Test")
 	    //res.send(result); //sends over the list of users
     })
 
@@ -67,10 +69,11 @@ app.post("/api/insert",(req,res) => {
     const backname = req.body.PassName
     const backpassword = req.body.PassPW
 
-    const sqlInsert = "INSERT INTO users (Name,Password) VALUES (?,?);" //mysql command to insert new elements 
+    const sqlInsert = "INSERT INTO users (user_first_name,password) VALUES (?,?);" //mysql command to insert new elements 
     db.query(sqlInsert,[backname,backpassword])
     });
 
 app.listen(3001, () => {
+    console.log("build directory: ", build_directory);
     console.log('running on port 3001')
 });
