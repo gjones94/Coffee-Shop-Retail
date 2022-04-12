6a0757e884399a492c6f007cfb0d023c7bdd2eab
Yyertsenoj94 <harvjones1994@gmail.com>
2022-04-12 14:02:34 -0500

    removing loading circle

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -71,16 +71,7 @@ function App() {
 
             </>}
 
-            {!data && <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
-                <Loader
-                    type="Rings"
-                    color="#6ac7ba"
-                    height={80}
-                    width={80}
-                    timeout={5000} />
-
-            </div>}
+            {!data} {/* Delay for data to load */}
 
         </>
     )
