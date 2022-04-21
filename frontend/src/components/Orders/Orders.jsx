ab80aae993f65e998a379c81529e356dd61d2f5e
cmw1999 <coop.m.win@gmail.com>
2022-04-21 01:18:17 -0500

    wrapping json in Orders

--- frontend/src/components/Orders/Orders.jsx
+++ frontend/src/components/Orders/Orders.jsx
@@ -168,7 +168,11 @@ function Orders ({admin}) {
                                     <div className="price">Date: {item.orders_date} </div>
                                     <div className="price">Order Total: ${item.orders_total} </div>
                                     <div className="price">User: {item.orders_user} </div>
-                                    <div className="price">Items: {item.orders_items} </div>
+                                    <pre>
+                                        <code>
+                                            <div className="price">Items: {item.orders_items} </div>
+                                        </code>
+                                    </pre>
                                 </div>
                             )
                         }
