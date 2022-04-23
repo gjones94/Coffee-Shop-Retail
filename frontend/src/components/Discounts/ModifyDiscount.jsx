ef872521eb27d1afe67d04e6452c61c0dddd846f
Kevin <71857556+kchan5782@users.noreply.github.com>
2022-04-22 20:09:06 -0500

    importing the correct discount css

--- frontend/src/components/Discounts/ModifyDiscount.jsx
+++ frontend/src/components/Discounts/ModifyDiscount.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-//import './ModifyDiscount.css';
+import './Discount.css';
 import Axios from 'axios';
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 
